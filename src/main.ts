07df263e1ef9e10b5280135faeddf583432c41dd
jackhuang <2504448153@qq.com>
2022-10-28 14:21:41 +0800

    feat: picker
    Signed-off-by: jackhuang <2504448153@qq.com>

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import * as vscode from "vscode";
+import * as vscode from 'vscode';
 
 interface ReplaceCommand {
     name: string;
@@ -8,30 +8,34 @@ interface ReplaceCommand {
 
 type ReplaceGroup = Array<ReplaceCommand>;
 
-
 async function transform() {
     const activeEditor = vscode.window.activeTextEditor;
     const group = vscode.workspace.getConfiguration('vscode-replace').get('commands') as ReplaceGroup;
-    if (!group) {
+    if (!group?.length) {
+        return
+    }
+
+    let items: vscode.QuickPickItem[] = group.map(item => ({ label: item.name }))
+
+    let selection = (await vscode.window.showQuickPick(items)) as vscode.QuickPickItem;
+    if (!selection) {
         return;
     }
 
-    const command = group[0];
+    let command = group.find((i) => i.name === selection.label) as ReplaceCommand;
     if (!command) {
         return;
     }
 
     if (!activeEditor) {
-      return;
+        return;
     }
 
     let doc = activeEditor.document.getText();
 
-
-    let result = doc.replace(new RegExp(command.match, 'g'), (text, $1, ...args) => {
+    let result = doc.replace(new RegExp(command.match, 'g'), (text, $1, $2, $3, ...args) => {
         return eval(command.replace);
     });
-    console.log('result', result);
 
     var firstLine = activeEditor.document.lineAt(0);
     var lastLine = activeEditor.document.lineAt(activeEditor.document.lineCount - 1);
@@ -39,10 +43,10 @@ async function transform() {
         0,
         firstLine.range.start.character,
         activeEditor.document.lineCount - 1,
-        lastLine.range.end.character
+        lastLine.range.end.character,
     );
 
-    activeEditor.edit(builder => {
+    activeEditor.edit((builder) => {
         builder.replace(textRange, result);
     });
 }
