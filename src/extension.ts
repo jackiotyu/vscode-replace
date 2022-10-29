2295698898b7a6abb53e548b0637ce423e3c64d5
jackiotyu <2504448153@qq.com>
2022-10-29 11:44:22 +0800

    feat: 添加icon、vm解析
    Signed-off-by: jackiotyu <2504448153@qq.com>

--- src/extension.ts
+++ src/extension.ts
@@ -2,11 +2,16 @@ import * as vscode from 'vscode';
 import { transform } from "./main";
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('vscode-replace.replace', () => {
-		transform();
-	});
+	// let transformFile = vscode.commands.registerCommand('vscode-replace.replace', () => {
+	// 	transform();
+	// });
 
-	context.subscriptions.push(disposable);
+	let textTransform = vscode.commands.registerTextEditorCommand('vscode-replace.replace', (editor) => {
+		transform(editor);
+	})
+
+	context.subscriptions.push(textTransform);
+	// context.subscriptions.push(transformFile);
 }
 
 export function deactivate() {}
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
 import * as vscode from 'vscode';
+import * as vm from 'vm';
+
+type KeyMap = Record<'result' | 'offset' | 'originText' | 'match', string>;
 
 interface ReplaceCommand {
     name: string;
@@ -7,37 +10,98 @@ interface ReplaceCommand {
     description?: string;
 }
 
+interface ReplaceSetting {
+    // 开启更多参数
+    moreParam?: boolean;
+    keyMap?: KeyMap;
+}
+
 type ReplaceGroup = Array<ReplaceCommand>;
 
-async function transform() {
-    const activeEditor = vscode.window.activeTextEditor;
+// 默认内部使用的键名
+const DEFAULT_RESULT_KEY = '__inner_res__';
+// 匹配到的子字符串在原字符串中的偏移量。（比如，如果原字符串是 'abcd'，匹配到的子字符串是 'bc'，那么这个参数将会是 1）
+const DEFAULT_OFFSET_KEY = '__offset__';
+// 被匹配的原字符串
+const DEFAULT_ORIGIN_TEXT_KEY = '__origin_text__';
+// 匹配的子串
+const DEFAULT_MATCH_KEY = '$&';
+
+const DEFAULT_SETTING = {
+    moreParam: false,
+    keyMap: {
+        result: DEFAULT_RESULT_KEY,
+        offset: DEFAULT_OFFSET_KEY,
+        originText: DEFAULT_ORIGIN_TEXT_KEY,
+        match: DEFAULT_MATCH_KEY,
+    },
+};
+
+// 获取需要执行的命令
+async function getCommand() {
     const group = vscode.workspace.getConfiguration('vscode-replace').get('commands') as ReplaceGroup;
     if (!group?.length) {
-        return
-    }
-
-    let items: vscode.QuickPickItem[] = group.map(item => ({ label: item.name, description: item.description }))
-
-    let selection = (await vscode.window.showQuickPick(items)) as vscode.QuickPickItem;
-    if (!selection) {
         return;
     }
+
+    let items: vscode.QuickPickItem[] = group.map((item) => ({ label: item.name, description: item.description }));
+    let selection = (await vscode.window.showQuickPick(items)) as vscode.QuickPickItem;
+    if (!selection) return;
 
     let command = group.find((i) => i.name === selection.label) as ReplaceCommand;
-    if (!command) {
-        return;
+    return command;
+}
+
+function getSetting(): ReplaceSetting {
+    const setting = vscode.workspace.getConfiguration('vscode-replace').get('setting');
+    return setting || DEFAULT_SETTING;
+}
+
+/**
+ * 获取替换后的内容
+ * @param command 设置的替换配置
+ * @param text 匹配的文本
+ * @param args
+ * @returns
+ */
+function getReplacement(command: ReplaceCommand, text: string, ...args: any[]) {
+    try {
+        const { replace } = command;
+        const { moreParam, keyMap } = getSetting();
+
+        let argGroup = args.slice(0, -2);
+        let offset = args[args.length - 2];
+        let originText = args[args.length - 1];
+        let resultKey = keyMap?.result || DEFAULT_RESULT_KEY;
+        let paramMap: Record<string, string> = argGroup.reduce((map, item, index) => {
+            map[`$${index + 1}`] = item;
+            return map;
+        }, {});
+        paramMap[resultKey] = text;
+
+        // 开启更多参数
+        if (moreParam) {
+            paramMap[keyMap?.match || DEFAULT_MATCH_KEY] = text;
+            paramMap[keyMap?.offset || DEFAULT_OFFSET_KEY] = offset;
+            paramMap[keyMap?.originText || DEFAULT_ORIGIN_TEXT_KEY] = originText;
+        }
+        // TODO 判断是否为js表达式
+
+        // 使用vm模块运行replace内容，获取运行结果
+        let context = vm.createContext(paramMap);
+        vm.runInContext(`${resultKey}=` + replace, context);
+        return context[resultKey];
+    } catch (error) {
+        return text;
     }
+}
 
-    if (!activeEditor) {
-        return;
-    }
-
-    let doc = activeEditor.document.getText();
-
-    let result = doc.replace(new RegExp(command.match, 'g'), (text, $1, $2, $3, ...args) => {
-        return eval(command.replace);
-    });
-
+/**
+ * 替换编辑器文本
+ * @param activeEditor 当前激活的编辑器
+ * @param text 需要替换的文本
+ */
+function handleReplace(activeEditor: vscode.TextEditor, text: string) {
     var firstLine = activeEditor.document.lineAt(0);
     var lastLine = activeEditor.document.lineAt(activeEditor.document.lineCount - 1);
     var textRange = new vscode.Range(
@@ -48,8 +112,29 @@ async function transform() {
     );
 
     activeEditor.edit((builder) => {
-        builder.replace(textRange, result);
+        builder.replace(textRange, text);
     });
+}
+
+async function transform(activeEditor: vscode.TextEditor) {
+    let command = (await getCommand()) as ReplaceCommand;
+    if (!command) return;
+    let doc = activeEditor.document.getText();
+    // TODO 内容匹配设定
+    // TODO 匹配内容高亮
+    let result = doc.replace(new RegExp(command.match, 'g'), (text, ...args) => {
+        return getReplacement(command, text, ...args);
+    });
+
+    // TODO 确认替换
+    let pickOptions = ['确认', '取消'];
+
+    let confirm = await vscode.window.showInformationMessage('确认替换？', ...pickOptions);
+
+    console.log(confirm, 'confirm');
+    if (confirm === pickOptions[0]) {
+        handleReplace(activeEditor, result);
+    }
 }
 
 export { transform };
