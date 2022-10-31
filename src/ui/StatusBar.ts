93fbe277ddd42910127ee7a764bace5fe2d8e5b1
jackiotyu <2504448153@qq.com>
2022-10-31 22:44:58 +0800

    perf: stausBar展示条件
    Signed-off-by: jackiotyu <2504448153@qq.com>

--- src/ui/StatusBar.ts
+++ src/ui/StatusBar.ts
@@ -2,12 +2,20 @@ import * as vscode from 'vscode';
 import { Command } from '../constants';
 
 export class StatusBar {
+    statusBar: vscode.StatusBarItem;
     constructor(context: vscode.ExtensionContext) {
         let statusBar = vscode.window.createStatusBarItem('replace.statusbar', vscode.StatusBarAlignment.Right, 200);
         statusBar.command = Command.REPLACE_EVENT;
         statusBar.tooltip = 'js replace';
         statusBar.text = 'js replace';
+        this.statusBar = statusBar;
         context.subscriptions.push(statusBar);
-        statusBar.show();
+        this.checkCanShow(vscode.window.activeTextEditor);
+        vscode.window.onDidChangeActiveTextEditor((editor) => this.checkCanShow(editor));
+    }
+
+    checkCanShow(editor: vscode.TextEditor | undefined): void {
+        if (editor) return this.statusBar.show();
+        this.statusBar.hide();
     }
 }
