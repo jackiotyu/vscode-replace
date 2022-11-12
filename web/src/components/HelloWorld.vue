400868afe58a0c80925729c08b4132921ef8d5d7
jackiotyu <2504448153@qq.com>
2022-11-12 23:43:02 +0800

    chore: webview-ui-toolkit
    Signed-off-by: jackiotyu <2504448153@qq.com>

--- web/src/components/HelloWorld.vue
+++ web/src/components/HelloWorld.vue
@@ -3,12 +3,23 @@
         <h1>{{ msg }}</h1>
         <button @click="triggerAdd">试试添加9</button>
         <button @click="triggerReload">刷新</button>
+        <!-- 注意命名规则 -->
+        <vscode-button @click="triggerReload">刷新</vscode-button>
+        <vscode-checkbox>选项</vscode-checkbox>
     </div>
 </template>
 
 <script setup lang="ts">
+import {
+    provideVSCodeDesignSystem,
+    vsCodeButton,
+    vsCodeCheckbox,
+} from '@vscode/webview-ui-toolkit';
 import { defineProps, ref } from 'vue';
 import { vscode } from '@/utils/common';
+
+// 注册组件
+provideVSCodeDesignSystem().register(vsCodeButton(), vsCodeCheckbox());
 
 const props = defineProps({
     msg: { type: String, required: true },
