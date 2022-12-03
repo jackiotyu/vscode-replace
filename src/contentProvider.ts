12399537a826ab560e8576bdc2a5c3da1f17e5fa
jackiotyu <2504448153@qq.com>
2022-12-03 19:27:57 +0800

    perf: 添加中断方法
    Signed-off-by: jackiotyu <2504448153@qq.com>

--- src/contentProvider.ts
+++ src/contentProvider.ts
@@ -10,17 +10,30 @@ export default class ContentProvider
     // 提供只读文档内容
     // TODO 添加缓存
     // TODO 监听文档修改事件
-    async provideTextDocumentContent(uri: vscode.Uri): Promise<string> {
-        let { query, path } = uri;
-        // 获取源文件内容
-        let document = await vscode.workspace.openTextDocument(path);
-        let docText = document.getText();
-        let { match, replace } = JSON.parse(query);
-        // 直接获取替换的完整文本
-        let replaceText = docText.replace(
-            RegExp(match, 'mg'),
-            (text, ...args) => getReplaceText(replace, text, ...args)
-        );
-        return replaceText;
+    async provideTextDocumentContent(
+        uri: vscode.Uri,
+        cancelToken: vscode.CancellationToken
+    ): Promise<string> {
+        try {
+            let { query, path } = uri;
+            // 获取源文件内容
+            let document = await vscode.workspace.openTextDocument(path);
+            let docText = document.getText();
+            let { match, replace } = JSON.parse(query);
+            // 直接获取替换的完整文本
+            let replaceText = docText.replace(
+                RegExp(match, 'mg'),
+                (text, ...args) => {
+                    if (cancelToken.isCancellationRequested) {
+                        throw new Error('跳出匹配');
+                    }
+                    return getReplaceText(replace, text, ...args);
+                }
+            );
+            return replaceText;
+        } catch (error) {
+            console.error('🚀 跳出匹配 >>', error);
+            return '';
+        }
     }
 }
